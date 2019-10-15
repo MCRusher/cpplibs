fc89d3793b65c36e84553dfeec0ee0d2f481b24d
MCRusher <Modernwarfare3Minecraft64@gmail.com>
2019-10-15 09:04:05 -0400

    Add files via upload

---
+++ cxmem.hpp
@@ -0,0 +1,338 @@
+#pragma once
+
+#include <cstdlib>
+#include <exception>
+#include <utility>
+#include <string>
+#include <iterator>
+#include <iostream>
+#include <cstdint>
+#include <cstring>
+#include <limits>
+
+namespace mem {
+
+    enum class mode {
+        COPY,//Copies the data
+        MOVE,//Moves as a T const&&
+        //UB_MOVE,//moves as a R&&, const_cast undefined behavior
+    };
+
+    enum class rawset {
+        NONE,
+        ZERO,
+    };
+
+    constexpr std::size_t npos = std::numeric_limits<std::size_t>::max();
+
+    class memex : public std::exception {
+        std::string _what;
+    public:
+        memex(std::string const& w) : _what(w) {}
+        const char * what()const noexcept{ return _what.c_str(); }
+    };
+
+    /*template <typename T>
+    T* alloc(std::size_t num){
+        if(num==0)
+            return nullptr;
+        T* tmp = (T*)std::malloc(num*sizeof(T)+sizeof(std::size_t));
+        if(!tmp)
+            throw memex("mem::alloc failed");
+        *((std::size_t*)tmp) = num;
+        tmp = (T*)(((std::size_t*)tmp)+1);
+        for(std::size_t i = 0; i < num; i++){
+            new (&tmp[i]) T{};
+        }
+        return tmp;
+    }*/
+
+    template <typename T, typename... Tx>
+    T* alloc(std::size_t num, Tx const&... args){
+        if(num==0)
+            return nullptr;
+        T* tmp = (T*)std::malloc(num*sizeof(T)+sizeof(std::size_t));
+        if(!tmp)
+            throw memex("mem::alloc failed");
+        *((std::size_t*)tmp) = num;
+        tmp = (T*)(((std::size_t*)tmp)+1);
+        for(std::size_t i = 0; i < num; i++){
+            new (&tmp[i]) T{args...};
+        }
+        return tmp;
+    }
+
+    template <typename T, mode Mode = mode::COPY>
+    T* alloc(std::initializer_list<T> il){
+        using ilsize = typename std::initializer_list<T>::size_type;
+        T* tmp = (T*)std::malloc(il.size()*sizeof(T)+sizeof(std::size_t));
+        if(!tmp)
+            throw memex("mem::alloc failed");
+        *((std::size_t*)tmp) = il.size();
+        tmp = (T*)(((std::size_t*)tmp)+1);
+        auto iter = il.begin();
+        for(ilsize i = 0; i < il.size(); i++){
+            if constexpr(Mode == mode::COPY){
+                new (&tmp[i]) T{*(iter++)};
+            }else if constexpr(Mode == mode::MOVE){
+                //will only work for T const&& move constructors
+                new (&tmp[i]) T{std::move(*(iter++))};
+            }/*else{
+                //Note that this is undefined behavior as
+                //iterator lists may be placed in read-only
+                //storage
+                new (&tmp[i]) T{const_cast<T&&>(std::move(*(iter++)))};
+            }*/
+        }
+        return tmp;
+    }
+
+    template <typename T>
+    std::size_t size(T const * const arr){
+        if(arr==nullptr)
+            return 0;
+        return *((std::size_t*)arr-1);
+    }
+
+    template <typename T>
+    void dealloc(T*& arr){
+        if(arr == nullptr)
+            return;
+        void* full_arr = ((std::size_t*)arr-1);
+        std::size_t old = *((std::size_t*)arr-1);
+        while(old>0){
+            arr[--old].~T();
+        }
+        std::free(full_arr);
+        arr = nullptr;
+    }
+
+    template <typename T>
+    void shrink(T*& arr, std::size_t num){
+        if(arr == nullptr || num == 0)
+            return;
+        std::size_t old = *((std::size_t*)arr-1);
+        if((((std::intmax_t)old)-((std::intmax_t)num)) < 0){
+            dealloc(arr);
+            return;
+        }
+        void* full_arr = ((std::size_t*)arr-1);
+        for(std::size_t i = old; i > old-num; --i){
+            arr[i].~T();
+        }
+        T* tmp = (T*)std::realloc(full_arr,(old-num)*sizeof(T)+sizeof(std::size_t));
+        if(!tmp)
+            throw memex("mem::shrink failed");
+        *((std::size_t*)tmp) = old-num;
+        arr = (T*)(((std::size_t*)tmp)+1);
+    }
+
+    template <typename T, typename... Tx>
+    void grow(T*& arr, std::size_t num, Tx const&... args){
+        if(arr == nullptr || num == 0)
+            return;
+        void* full_arr = ((std::size_t*)arr-1);
+        std::size_t old = *((std::size_t*)arr-1);
+        T* tmp = (T*)std::realloc(full_arr,(old+num)*sizeof(T)+sizeof(std::size_t));
+        if(!tmp)
+            throw memex("mem::grow failed");
+        *((std::size_t*)tmp) = old+num;
+        arr = (T*)(((std::size_t*)tmp)+1);
+        for(std::size_t i = old; i < old+num; i++){
+            new (&arr[i]) T{args...};
+        }
+    }
+
+    template <mode Mode = mode::COPY, typename T>
+    void grow(T*& arr, std::initializer_list<T> il){
+        if(arr == nullptr || il.size() == 0)
+            return;
+        void* full_arr = ((std::size_t*)arr-1);
+        std::size_t old = *((std::size_t*)arr-1);
+        T* tmp = (T*)std::realloc(full_arr,(old+il.size())*sizeof(T));
+        if(!tmp)
+            throw memex("mem::grow failed");
+        *((std::size_t*)tmp) = old+il.size();
+        arr = (T*)(((std::size_t*)tmp)+1);
+        auto iter = il.begin();
+        for(std::size_t i = old; i < old+il.size(); i++){
+            if constexpr(Mode == mode::COPY){
+                new (&tmp[i]) T{*(iter++)};
+            }else if constexpr(Mode == mode::MOVE){
+                //will only work for T const&& move constructors
+                new (&tmp[i]) T{std::move(*(iter++))};
+            }/*else{
+                //Note that this is undefined behavior as
+                //iterator lists may be placed in read-only
+                //storage
+                new (&tmp[i]) T{const_cast<T&&>(std::move(*(iter++)))};
+            }*/
+        }
+    }
+
+    template <typename T>
+    void copy(T*& arr, T const * const src){
+        std::size_t num = size(arr);
+        for(std::size_t i = 0; i < num; i++){
+            arr[i].~T();
+            new (&arr[i]) T{src[i]};
+        }
+    }
+
+    template <typename T>
+    T* copy(T const * const arr){
+        std::size_t num = size(arr);
+        T* tmp = (T*)std::malloc(num*sizeof(T)+sizeof(std::size_t));
+        if(!tmp)
+            throw memex("mem::copy failed");
+        *((std::size_t*)tmp) = num;
+        tmp = (T*)(((std::size_t*)tmp)+1);
+        for(std::size_t i = 0; i < num; i++){
+            new (&tmp[i]) T{arr[i]};
+        }
+        return tmp;
+    }
+
+    template <typename T, typename... Tx>
+    void set(T*& arr, std::size_t pos, std::size_t num, Tx const&... args){
+        if(arr == nullptr || num == 0)
+            return;
+        if(num == npos)
+            num = size(arr);
+        for(std::size_t i = pos; i < pos+num; i++){
+            arr[i].~T();
+            new (&arr[i]) T{args...};
+        }
+    }
+
+    template <mode Mode = mode::COPY, typename T>
+    void set(T*& arr, std::size_t pos, std::initializer_list<T> il){
+        if(arr == nullptr || il.size() == 0)
+            return;
+        auto iter = il.begin();
+        for(std::size_t i = pos; i < pos+il.size(); i++){
+            arr[i].~T();
+            if constexpr(Mode == mode::COPY){
+                new (&arr[i]) T{*(iter++)};
+            }else if constexpr(Mode == mode::MOVE){
+                //will only work for T const&& move constructors
+                new (&arr[i]) T{std::move(*(iter++))};
+            }else{
+                //Note that this is undefined behavior as
+                //iterator lists may be placed in read-only
+                //storage
+                new (&arr[i]) T{const_cast<T&&>(std::move(*(iter++)))};
+            }
+        }
+    }
+
+
+    template <mode Mode = mode::COPY, typename T>
+    void set(T*& arr, std::initializer_list<T> il){
+        if(arr == nullptr || il.size() == 0)
+            return;
+        auto iter = il.begin();
+        for(std::size_t i = 0; i < il.size(); i++){
+            arr[i].~T();
+            if constexpr(Mode == mode::COPY){
+                new (&arr[i]) T{*(iter++)};
+            }else if constexpr(Mode == mode::MOVE){
+                //will only work for T const&& move constructors
+                new (&arr[i]) T{std::move(*(iter++))};
+            }else{
+                //Note that this is undefined behavior as
+                //iterator lists may be placed in read-only
+                //storage
+                new (&arr[i]) T{const_cast<T&&>(std::move(*(iter++)))};
+            }
+        }
+    }
+
+    template <typename T>
+    void move(T*& arr, T* const src){
+        std::size_t num = size(arr);
+        for(std::size_t i = 0; i < num; i++){
+            arr[i].~T();
+            new (&arr[i]) T{std::move(src[i])};
+        }
+    }
+
+    //void* specializations
+
+    template <rawset RawSet = rawset::NONE>
+    void* alloc(std::size_t bytes){
+        if(bytes==0)
+            return nullptr;
+        //sizeof(void) is UB, but is usually expected to be 1 byte
+        void* tmp;
+        if constexpr(RawSet == rawset::NONE){
+            tmp = std::malloc(bytes+sizeof(std::size_t));
+        }else{
+            tmp = std::calloc(bytes+sizeof(std::size_t),1);
+        }
+        if(!tmp)
+            throw memex("mem::alloc<void*> failed");
+        *((std::size_t*)tmp) = bytes;
+        tmp = (void*)(((std::size_t*)tmp)+1);
+        return tmp;
+    }
+
+    template<>
+    void dealloc<void>(void*& rawarr){
+        if(rawarr == nullptr)
+            return;
+        void* full_arr = ((std::size_t*)rawarr-1);
+        std::free(full_arr);
+        rawarr = nullptr;
+    }
+
+    template <>
+    void shrink<void>(void*& rawarr, std::size_t num){
+        if(rawarr == nullptr || num == 0)
+            return;
+        std::size_t old = *((std::size_t*)rawarr-1);
+        if((((std::intmax_t)old)-((std::intmax_t)num)) < 0){
+            dealloc(rawarr);
+            return;
+        }
+        void* full_arr = ((std::size_t*)rawarr-1);
+        //sizeof(void) is UB, but is usually expected to be 1 byte
+        void* tmp = std::realloc(full_arr,(old-num)+sizeof(std::size_t));
+        if(!tmp)
+            throw memex("mem::shrink<void*> failed");
+        *((std::size_t*)tmp) = old-num;
+        rawarr = (void*)(((std::size_t*)tmp)+1);
+    }
+
+    template <rawset RawSet = rawset::NONE>
+    void grow(void*& arr, std::size_t num){
+        if(arr == nullptr || num == 0)
+            return;
+        void* full_arr = ((std::size_t*)arr-1);
+        std::size_t old = *((std::size_t*)arr-1);
+        void* tmp = std::realloc(full_arr,(old+num)+sizeof(std::size_t));
+        if(!tmp)
+            throw memex("mem::grow<void*> failed");
+        *((std::size_t*)tmp) = old+num;
+        arr = (void*)(((std::size_t*)tmp)+1);
+        if constexpr(RawSet == rawset::ZERO)
+            std::memset(arr+old,0,num);
+    }
+
+    void copy(void*& arr, void const * const src){
+        std::size_t num = size(arr);
+        std::memcpy(arr,src,num);
+    }
+
+    template <typename T>
+    void* copy(void const * const arr){
+        std::size_t num = size(arr);
+        T* tmp = (T*)std::malloc(num+sizeof(std::size_t));
+        if(!tmp)
+            throw memex("mem::copy<void*> failed");
+        *((std::size_t*)tmp) = num;
+        tmp = (T*)(((std::size_t*)tmp)+1);
+        std::memcpy(tmp,arr,num);
+        return tmp;
+    }
+}
---
+++ simpleio.hpp
@@ -0,0 +1,497 @@
+#include <ios>
+#include <iostream>
+#include <string>
+#include <exception>
+#include <sstream>
+#include <limits>
+#include <type_traits>
+
+namespace io {
+    class ioex : public std::exception {
+        std::string _what;
+    public:
+        ioex(std::string const& w) : _what(w) {}
+        const char * what()const noexcept{ return _what.c_str(); }
+    };
+
+    enum class mode {
+        RETRY,
+        THROW,
+    };
+
+    template<typename T1, typename... Tx>
+    std::enable_if_t<!std::is_base_of_v<std::ostream, T1>>
+    println(T1 const& arg, Tx const&... args){
+        ((std::cout << arg) << ... << args) << std::endl;
+    }
+    template<typename T1, typename... Tx>
+    std::enable_if_t<!std::is_base_of_v<std::ostream, T1>>
+    print(T1 const& arg, Tx const&... args){
+        ((std::cout << arg) << ... << args) << std::flush;
+    }
+    template<typename... Tx>
+    void println(std::ostream& ostrm, Tx const&... args){
+        (ostrm << ... << args) << std::endl;
+    }
+    template<typename... Tx>
+    void print(std::ostream& ostrm, Tx const&... args){
+        (ostrm << ... << args) << std::flush;
+    }
+
+    template<typename T1, typename... Tx>
+    std::enable_if_t<!std::is_base_of_v<std::wostream, T1>>
+    wprintln(T1 const& arg, Tx const&... args){
+        ((std::wcout << arg) << ... << args) << std::endl;
+    }
+    template<typename T1, typename... Tx>
+    std::enable_if_t<!std::is_base_of_v<std::wostream, T1>>
+    wprint(T1 const& arg, Tx const&... args){
+        ((std::wcout << arg) << ... << args) << std::flush;
+    }
+    template<typename... Tx>
+    void wprintln(std::wostream& wostrm, Tx const&... args){
+        (wostrm << ... << args) << std::endl;
+    }
+    template<typename... Tx>
+    void print(std::wostream& wostrm, Tx const&... args){
+        (wostrm << ... << args) << std::flush;
+    }
+
+    //input
+
+    std::string readln(){
+        std::string res;
+        std::getline(std::cin,res);
+        return res;
+    }
+    std::string readln(std::istream& istrm){
+        std::string res;
+        std::getline(istrm,res);
+        return res;
+    }
+
+    void readln(std::string& res){
+        std::getline(std::cin,res);
+    }
+    void readln(std::istream& istrm, std::string& res){
+        std::getline(istrm,res);
+    }
+
+    template<mode Mode, typename T1, typename... Tx>
+    std::enable_if_t<!std::is_base_of_v<std::istream, T1>>
+    readln(T1& arg, Tx&... args){
+        std::string tmp;
+        if constexpr(Mode == mode::THROW){
+            std::getline(std::cin,tmp);
+            std::istringstream res(tmp);
+            if(!(res >> arg)){
+                std::ostringstream buf;
+                buf << "readln(std::cin,...) failed for typeid \""
+                << typeid(arg).name()
+                << "\" with "
+                << sizeof...(args)
+                << " other arguments remaining.";
+                throw ioex(buf.str());
+            }
+        }else{
+            std::getline(std::cin,tmp);
+            std::istringstream res(tmp);
+            while(!(res >> arg)){
+                res.clear();
+                std::getline(std::cin,tmp);
+                res.str(tmp);
+            }
+        }
+
+        if constexpr(sizeof...(args)!=0)
+            readln<Mode>(args...);
+    }
+    template<mode Mode, typename... Tx>
+    void readln(bool& arg, Tx&... args){
+        std::string tmp;
+        if constexpr(Mode == mode::THROW){
+            std::getline(std::cin,tmp);
+            if(tmp!="true" && tmp!="false"){
+                std::ostringstream buf;
+                buf << "readln(std::cin,bool,...) failed for typeid \""
+                << typeid(arg).name()
+                << "\" with "
+                << sizeof...(args)
+                << " other arguments remaining.";
+                throw ioex(buf.str());
+            }
+            arg = (tmp=="true" ? true : false);
+        }else{
+            std::getline(std::cin,tmp);
+            while(tmp!="true" && tmp!="false"){
+                std::getline(std::cin,tmp);
+            }
+            arg = (tmp=="true" ? true : false);
+        }
+
+        if constexpr(sizeof...(args)!=0)
+            readln<Mode>(args...);
+    }
+    template<mode Mode, typename T1, typename... Tx>
+    void readln(std::istream& istrm, T1& arg, Tx&... args){
+        std::string tmp;
+        if constexpr(Mode == mode::THROW){
+            std::getline(istrm,tmp);
+            std::istringstream res(tmp);
+            if(!(res >> arg)){
+                std::ostringstream buf;
+                buf << "readln(std::istream,...) failed for typeid \""
+                << typeid(arg).name()
+                << "\" with "
+                << sizeof...(args)
+                << " other arguments remaining.";
+                throw ioex(buf.str());
+            }
+        }else{
+            std::getline(istrm,tmp);
+            std::istringstream res(tmp);
+            while(!(res >> arg)){
+                res.clear();
+                std::getline(istrm,tmp);
+                res.str(tmp);
+            }
+        }
+
+        if constexpr(sizeof...(args)!=0)
+            readln<Mode>(istrm,args...);
+    }
+    template<mode Mode, typename... Tx>
+    void readln(std::istream& istrm, bool& arg, Tx&... args){
+        std::string tmp;
+        if constexpr(Mode == mode::THROW){
+            std::getline(istrm,tmp);
+            if(tmp!="true" && tmp!="false"){
+                std::ostringstream buf;
+                buf << "readln(std::istream,bool,...) failed for typeid \""
+                << typeid(arg).name()
+                << "\" with "
+                << sizeof...(args)
+                << " other arguments remaining.";
+                throw ioex(buf.str());
+            }
+            arg = (tmp=="true" ? true : false);
+        }else{
+            std::getline(istrm,tmp);
+            while(tmp!="true" && tmp!="false"){
+                std::getline(istrm,tmp);
+            }
+            arg = (tmp=="true" ? true : false);
+        }
+
+        if constexpr(sizeof...(args)!=0)
+            readln<Mode>(istrm,args...);
+    }
+
+    template<typename T1, typename... Tx>
+    std::enable_if_t<!std::is_base_of_v<std::istream, T1>>
+    readln(T1& arg, Tx&... args){
+        readln<mode::RETRY>(arg,args...);
+    }
+    template<typename T1, typename... Tx>
+    void readln(std::istream& istrm, T1& arg, Tx&... args){
+        readln<mode::RETRY>(istrm,arg,args...);
+    }
+
+    char readchar(){
+        char c = std::cin.get();
+        if(!std::cin)
+            throw ioex("io::readchar(std::cin) failed");
+        return c;
+    }
+    char readchar(std::istream& istrm){
+        char c = istrm.get();
+        if(!istrm)
+            throw ioex("io::readchar(std::istream) failed");
+        return c;
+    }
+
+    std::string read(){
+        std::string res;
+        std::cin >> res;
+        std::cin.ignore();
+        return res;
+    }
+    std::string read(std::istream& istrm){
+        std::string res;
+        istrm >> res;
+        istrm.ignore();
+        return res;
+    }
+
+    void read(std::string& res){
+        std::cin >> res;
+        std::cin.ignore();
+    }
+    void read(std::istream& istrm, std::string& res){
+        istrm >> res;
+        istrm.ignore();
+    }
+
+    template<mode Mode, typename T1, typename... Tx>
+    std::enable_if_t<!std::is_base_of_v<std::istream, T1>>
+    read(T1& arg, Tx&... args){
+        if constexpr(Mode == mode::THROW){
+            if(!(std::cin >> arg)){
+                std::cin.clear();
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+                std::ostringstream buf;
+                buf << "read(std::cin,...) failed for typeid \""
+                << typeid(arg).name()
+                << "\" with "
+                << sizeof...(args)
+                << " other arguments remaining.";
+                throw ioex(buf.str());
+            }
+        }else{
+            while(!(std::cin >> arg)){
+                //clears the entire line and resets stream state
+                if(!std::cin){
+                    std::cin.clear();
+                    std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+                }
+            }
+        }
+        //the template recursive base case
+        if constexpr(sizeof...(args)!=0)
+            read<Mode>(args...);
+        else
+            std::cin.ignore();
+    }
+    template<mode Mode, typename T1, typename... Tx>
+    void read(std::istream& istrm, T1& arg, Tx&... args){
+        if constexpr(Mode == mode::THROW){
+            if(!(istrm >> arg)){
+                istrm.clear();
+                istrm.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+                std::ostringstream buf;
+                buf << "read(std::istream,...) failed for typeid \""
+                << typeid(arg).name()
+                << "\" with "
+                << sizeof...(args)
+                << " other arguments remaining.";
+                throw ioex(buf.str());
+            }
+        }else{
+            while(!(istrm >> arg)){
+                //clears the entire line and resets stream state
+                if(!istrm){
+                    istrm.clear();
+                    istrm.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+                }
+            }
+        }
+        //the template recursive base case
+        if constexpr(sizeof...(args)!=0)
+            read<Mode>(istrm,args...);
+        else
+            istrm.ignore();
+    }
+
+    template<typename T1, typename... Tx>
+    std::enable_if_t<!std::is_base_of_v<std::istream, T1>>
+    read(T1& arg, Tx&... args){
+        read<mode::RETRY>(arg,args...);
+    }
+    template<typename T1, typename... Tx>
+    void read(std::istream& istrm, T1& arg, Tx&... args){
+        read<mode::RETRY>(istrm,arg,args...);
+    }
+
+    //wide read versions
+
+    std::wstring wreadln(){
+        std::wstring res;
+        std::getline(std::wcin,res);
+        return res;
+    }
+    std::wstring wreadln(std::wistream& wistrm){
+        std::wstring res;
+        std::getline(wistrm,res);
+        return res;
+    }
+
+    void wreadln(std::wstring& res){
+        std::getline(std::wcin,res);
+    }
+    void wreadln(std::wistream& wistrm, std::wstring& res){
+        std::getline(wistrm,res);
+    }
+
+    template<mode Mode, typename T1, typename... Tx>
+    std::enable_if_t<!std::is_base_of_v<std::wistream, T1>>
+    wreadln(T1& arg, Tx&... args){
+        std::wstring tmp;
+        if constexpr(Mode == mode::THROW){
+            std::getline(std::wcin,tmp);
+            std::wistringstream res(tmp);
+            if(!(res >> arg)){
+                std::ostringstream buf;
+                buf << "wreadln(std::wcin,...) failed for typeid \""
+                << typeid(arg).name()
+                << "\" with "
+                << sizeof...(args)
+                << " other arguments remaining.";
+                throw ioex(buf.str());
+            }
+        }else{
+            std::getline(std::wcin,tmp);
+            std::wistringstream res(tmp);
+            while(!(res >> arg)){
+                res.clear();
+                std::getline(std::wcin,tmp);
+                res.str(tmp);
+            }
+        }
+
+        if constexpr(sizeof...(args)!=0)
+            wreadln<Mode>(args...);
+    }
+    template<mode Mode, typename T1, typename... Tx>
+    void wreadln(std::wistream& wistrm, T1& arg, Tx&... args){
+        std::wstring tmp;
+        if constexpr(Mode == mode::THROW){
+            std::getline(wistrm,tmp);
+            std::wistringstream res(tmp);
+            if(!(res >> arg)){
+                std::ostringstream buf;
+                buf << "wreadln(std::wistream,...) failed for typeid \""
+                << typeid(arg).name()
+                << "\" with "
+                << sizeof...(args)
+                << " other arguments remaining.";
+                throw ioex(buf.str());
+            }
+        }else{
+            std::getline(wistrm,tmp);
+            std::wistringstream res(tmp);
+            while(!(res >> arg)){
+                res.clear();
+                std::getline(wistrm,tmp);
+                res.str(tmp);
+            }
+        }
+
+        if constexpr(sizeof...(args)!=0)
+            wreadln<Mode>(wistrm,args...);
+    }
+
+    template<typename T1, typename... Tx>
+    std::enable_if_t<!std::is_base_of_v<std::wistream, T1>>
+    wreadln(T1& arg, Tx&... args){
+        wreadln<mode::RETRY>(arg,args...);
+    }
+    template<typename T1, typename... Tx>
+    void wreadln(std::wistream& wistrm, T1& arg, Tx&... args){
+        wreadln<mode::RETRY>(wistrm,arg,args...);
+    }
+
+    char wreadchar(){
+        wchar_t c = std::wcin.get();
+        if(!std::wcin)
+            throw ioex("io::wreadchar(std::wcin) failed");
+        return c;
+    }
+    char wreadchar(std::wistream& wistrm){
+        char c = wistrm.get();
+        if(!wistrm)
+            throw ioex("io::readchar(std::wistream) failed");
+        return c;
+    }
+
+    std::wstring wread(){
+        std::wstring res;
+        std::wcin >> res;
+        std::wcin.ignore();
+        return res;
+    }
+    std::wstring wread(std::wistream& wistrm){
+        std::wstring res;
+        wistrm >> res;
+        wistrm.ignore();
+        return res;
+    }
+
+    void wread(std::wstring& res){
+        std::wcin >> res;
+        std::wcin.ignore();
+    }
+    void read(std::wistream& wistrm, std::wstring& res){
+        wistrm >> res;
+        wistrm.ignore();
+    }
+
+    template<mode Mode, typename T1, typename... Tx>
+    std::enable_if_t<!std::is_base_of_v<std::wistream, T1>>
+    wread(T1& arg, Tx&... args){
+        if constexpr(Mode == mode::THROW){
+            if(!(std::wcin >> arg)){
+                std::wcin.clear();
+                std::wcin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+                std::ostringstream buf;
+                buf << "wread(std::wcin,...) failed for typeid \""
+                << typeid(arg).name()
+                << "\" with "
+                << sizeof...(args)
+                << " other arguments remaining.";
+                throw ioex(buf.str());
+            }
+        }else{
+            while(!(std::wcin >> arg)){
+                //clears the entire line and resets stream state
+                if(!std::wcin){
+                    std::wcin.clear();
+                    std::wcin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+                }
+            }
+        }
+        //the template recursive base case
+        if constexpr(sizeof...(args)!=0)
+            wread<Mode>(args...);
+        else
+            std::wcin.ignore();
+    }
+    template<mode Mode, typename T1, typename... Tx>
+    void wread(std::wistream& wistrm, T1& arg, Tx&... args){
+        if constexpr(Mode == mode::THROW){
+            if(!(wistrm >> arg)){
+                wistrm.clear();
+                wistrm.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+                std::ostringstream buf;
+                buf << "wread(std::wistream,...) failed for typeid \""
+                << typeid(arg).name()
+                << "\" with "
+                << sizeof...(args)
+                << " other arguments remaining.";
+                throw ioex(buf.str());
+            }
+        }else{
+            while(!(wistrm >> arg)){
+                //clears the entire line and resets stream state
+                if(!wistrm){
+                    wistrm.clear();
+                    wistrm.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+                }
+            }
+        }
+        //the template recursive base case
+        if constexpr(sizeof...(args)!=0)
+            wread<Mode>(wistrm,args...);
+        else
+            wistrm.ignore();
+    }
+
+    template<typename T1, typename... Tx>
+    std::enable_if_t<!std::is_base_of_v<std::wistream, T1>>
+    wread(T1& arg, Tx&... args){
+        wread<mode::RETRY>(arg,args...);
+    }
+    template<typename T1, typename... Tx>
+    void wread(std::wistream& wistrm, T1& arg, Tx&... args){
+        wread<mode::RETRY>(wistrm,arg,args...);
+    }
+}
